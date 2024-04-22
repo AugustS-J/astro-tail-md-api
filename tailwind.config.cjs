0de41e8b0cdf6614988521d41efbdf0ff78a72aa
AugustS-J <ausc0001@stud.kea.dk>
2024-04-22 19:20:54 +0200

    First commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [
@@ -11,9 +14,21 @@ module.exports = {
         extend: {
             colors: {
                 'persianmosaic': {
-                    500: '#196673',
+                    100: '#196673',
+                    200: '#367480',
+                    300: '#4e838e',
+                    400: '#64929b',
+                    500: '#7aa1a9',
+                    600: '#90b0b7',
+                    700: '#bbcfd3',
+                    800: '#d2dfe2',
+                    900: '#e8eff0',
                   },
-            }
-        }
+            },
+            fontFamily: {
+                'sans': ['"Prompt"', ...defaultTheme.fontFamily.sans],
+                'display': ['"Roboto"', 'monospace'], // Adds a new `font-display` class
+            },
+        },
     },
   };
